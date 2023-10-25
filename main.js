0d30cbe34b3dbafa0754400cd1cb65ecaebcdad5
JVZambao <joao.zambao@pucpr.edu.br>
2023-10-25 08:38:59 -0300

    Update main.js
    First update

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 const canvas= document.getElementById("tela")
 const ctx = canvas.getContext("2d")
 
+let lastTime = performance.now()
+
+
+const mainLoop = () => {
+//tamanho do canvas
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+
+//calcular o delta time
+
+    const now = performance.now
+    const deltaTime = now - lastTime
+    lastTime = now
+
+    for(let existencia of Existencias) {}
+
+    requestAnimationFrame(mainLoop)
+}
+
+mainLoop()
